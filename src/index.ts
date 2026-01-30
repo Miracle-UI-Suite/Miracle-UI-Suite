ced01793f2019c12c8c41fbae5db984bd288cdda
Sheikh Abdul Aziz <sheikhabdulaziz.ml@gmail.com>
2026-01-30 11:18:48 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /** 
  * MIT License
  * 
- * Copyright (c) [2025] [Zyron, Head Quarters] {@link https://github.com/Zyron-HQ}.
+ * Copyright (c) [2025-2026] [Alchemyst, Labs] {@link https://github.com/alchemyst-labs}.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
