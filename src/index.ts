0943035bd5ad883060bc0262808a876b238a9a9f
Sheikh Abdul Aziz <sheikhabdulaziz.ml@gmail.com>
2026-01-31 10:49:13 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /** 
  * MIT License
  * 
- * Copyright (c) [2025-2026] [Alchemyst, Labs] {@link https://github.com/alchemyst-labs}.
+ * Copyright (c) [2025-2026] [Alchemyst, Developers] {@link https://github.com/alchemyst-devs}.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
